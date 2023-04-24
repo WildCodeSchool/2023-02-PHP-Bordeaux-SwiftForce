fc09e54d33e695680d9286a2e1dc36a18d991e16
Gibon_david <[My\_gibon.david@live.fr](mailto:MY_gibon.david@live.fr)>
2023-04-24 11:30:07 +0200

    typage function

--- src/Model/ProductManager.php
+++ src/Model/ProductManager.php
@@ -8,11 +8,11 @@ class ProductManager extends AbstractManager
 {
     public const TABLE = 'WS_product';
 
-    public function sortByPrice(string $price) : bool|array
+    public function sortByPrice(string $price): bool|array
     {
         $sql = "SELECT * FROM WS_product ORDER BY " . $price;
         $stm = $this->pdo->prepare($sql);
-       /* $stm->bindValue('price', $price,/PDO::PARAM_STR);*/
+        /* $stm->bindValue('price', $price,/PDO::PARAM_STR);*/
         $stm->execute();
         return $stm->fetchAll(PDO::FETCH_ASSOC);
     }
