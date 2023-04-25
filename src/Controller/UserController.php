cc73130426757c6362416f28fc1c9d24f356a7ee
Gibon_david <[My\_gibon.david@live.fr](mailto:MY_gibon.david@live.fr)>
2023-04-25 15:46:45 +0200

    add fichier form

---
+++ src/Controller/UserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\UserManager;
+
+class UserController extends AbstractController
+{
+
+}
---
+++ src/Model/UserManager.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Model;
+
+class UserManager extends AbstractManager
+{
+
+
+}
