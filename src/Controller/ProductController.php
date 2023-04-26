d9445604d2705d84f9ffe30533f9bbd7891ce5a1
ETIENNE_JOUAULT <etienne.jouaulmt@live.fr>
2023-04-26 16:55:57 +0200

    show one product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,4 +13,12 @@ class ProductController extends AbstractController
 
         return $this->twig->render('Product/index.html.twig', ['products' => $products]);
     }
+
+    public function show(int $id): string
+    {
+        $productManager = new ProductManager();
+        $product = $productManager->selectOneById($id);
+
+        return $this->twig->render('product/show.html.twig', ['product' => $product]);
+    }
 }
