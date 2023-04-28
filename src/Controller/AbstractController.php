18809557f88b74755179afebe8c6a34c6490d8ed
ETIENNE_JOUAULT <etienne.jouaulmt@live.fr>
2023-04-28 09:45:52 +0200

    update header.html.twig

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Model\UserManager;
 use Twig\Environment;
 use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
@@ -12,6 +13,7 @@ use Twig\Loader\FilesystemLoader;
 abstract class AbstractController
 {
     protected Environment $twig;
+    protected array | false $user;
 
     public function __construct()
     {
@@ -24,6 +26,12 @@ abstract class AbstractController
             ]
         );
         $this->twig->addExtension(new DebugExtension());
+
+        $userManager = new UserManager();
+        $this->user = isset($_SESSION['user_id']) ? $userManager->selectOneById($_SESSION['user_id']) : false;
+
+        $this->twig->addGlobal('user', $this->user);
+
         if (isset($_SESSION['cart'])) {
             $this->twig->addGlobal('carts', $_SESSION['cart']);
         }
---
+++ src/Controller/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\UserManager;
+
+class LoginController extends AbstractController
+{
+    public function login()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $userManager = new UserManager();
+            $email = $_POST['email'];
+            $user = $userManager->getUserByEmail($email);
+            $_SESSION['user'] = $user;
+
+            if ($_POST['email'] === $user['email'] && password_verify($_POST['WS_password'], $user['WS_password'])) {
+                $_SESSION['user_id'] = $user['id'];
+                header('Location: /');
+            } else {
+                echo 'you are bab !';
+            }
+        }
+        return $this->twig->render('login/login.html.twig');
+    }
+
+    public function logout(): void
+    {
+        unset($_SESSION['user_id']);
+        header('location: /');
+    }
+}
